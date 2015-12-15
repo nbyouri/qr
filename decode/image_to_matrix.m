a716dfc93ac8cad9aa2a0c98d86c90cfdab4cab4
Samuel Monroe <spat.monroe@gmail.com>
2015-12-15 09:58:53 +0100

    Correction

--- decode/image_to_matrix.m
+++ decode/image_to_matrix.m
@@ -1,15 +1,15 @@
 % CODE DECODER
 
-% Ouvrir l'image du qrcode dans une matrice
+% Ouvrir limage du qrcode dans une matrice
 
-qrcode = imread('dirty.jpg');
+qrcode = imread('dirty.png');
 
 % Mise à niveau des blancs et noirs
 % Fonction MatLab qui prend une image en niveau de gris et applique un niveau de treshold pour retourner une image en noir et blanc, la matrice retournée est une matrice LOGIQUE*
 
 qr = im2bw(qrcode, 0.5)
 
-* Lissage via un filtre médian (plus adapté que le linéaire pour le bruit impulsionnel)
+% Lissage via un filtre médian (plus adapté que le linéaire pour le bruit impulsionnel)
 
 clean = medfilt2(qr);
 
